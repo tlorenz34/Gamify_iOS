ff9974086409296aee5bb10dac669420a1fdb0da
Thaddeus Lorenz <thaddeuslorenz@gmail.com>
2021-09-20 06:38:36 -0700

    Initial Commit

---
+++ Gamify_v1Tests/Gamify_v1Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Gamify_v1Tests.swift
+//  Gamify_v1Tests
+//
+//  Created by Thaddeus Lorenz on 9/20/21.
+//
+
+import XCTest
+@testable import Gamify_v1
+
+class Gamify_v1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
